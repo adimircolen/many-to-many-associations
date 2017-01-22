121163b9b500251c934cebdcc7bbdcb4e3135d19
Adimir Colen <adimircolen@gmail.com>
2017-01-22 10:29:54 -0200

    some commands to examplefy

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+p 'herman = Author.create name: "Herman Melville"'
+herman = Author.create name: "Herman Melville"
+p 'moby = Book.create title: "Moby Dick"'
+moby = Book.create title: "Moby Dick"
+p "herman.books << moby"
+herman.books << moby
+p "moby.authors"
+moby.authors
+p "herman.books"
+herman.books
+p "herman.books.where(title: 'Moby Dick')"
+herman.books.where(title: 'Moby Dick')
